feaecae3ccdb2a7769b8fc4db2409cf8cc31e58e
Jason Yang <git@jasony.org>
2025-03-14 16:09:04 -0700

    refactor: cmake source list script update

--- getIncludeList.sh
+++ getIncludeList.sh
@@ -6,22 +6,53 @@ APP_PATH='Application/*'
 DEP_LIB_PATH='Application/Inc/*'
 CORE_PATH='Core/*'
 DRIVERS_PATH='Drivers/*'
-FATFS_PATH='FATFS/*'
+# FATFS_PATH='FATFS/*'
 MIDWARE_PATH='Middlewares/*'
-RTOS='Middlewares/Third_Party/FreeRTOS/*'
-USB_PATH='USB_Device/*'
+RTOS_PATH='Middlewares/Third_Party/FreeRTOS/*'
+# USB_PATH='USB_Device/*'
 
-APP_INC="$(eval find "$APP_PATH" -type d -name 'Inc')"
-DEP_LIB_INC="$(eval find "$DEP_LIB_PATH" -type d)"
-CORE_INC="$(eval find "$CORE_PATH" -type d -name 'Inc' -o -name 'Include')"
-DRIVERS_INC="$(eval find "$DRIVERS_PATH" -type d -name 'Inc' -o -name 'Include' -o -name 'Legacy')"
-RTOS="$(eval find "$MIDWARE_PATH" -type d -name 'include' -o -name 'CMSIS_RTOS' -o -name 'ARM_CM4F')"
-MIDWARE_INC="$(eval find "$MIDWARE_PATH" -type d -name 'Inc' -o -name 'src')"
-USB_INC="$(eval find "$USB_PATH" -type d)"
-FATFS_INC="$(eval find "$FATFS_PATH" -type d)"
+# Clear the file
+> cmake/IncludeList.cmake
 
-# Print find result
-printf "$APP_INC\n$DEP_LIB_INC\n$CORE_INC\n$DRIVERS_INC\n$RTOS\n$MIDWARE_INC\n$USB_INC\n$FATFS_INC" > cmake/IncludeList.cmake
+if [ -n "$APP_PATH" ]; then
+	APP_INC="$(eval find "$APP_PATH" -type d -name 'Inc')"
+	printf "$APP_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$DEP_LIB_PATH" ]; then
+	DEP_LIB_INC="$(eval find "$DEP_LIB_PATH" -type d)"
+	printf "$DEP_LIB_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$CORE_PATH" ]; then
+	CORE_INC="$(eval find "$CORE_PATH" -type d -name 'Inc' -o -name 'Include')"
+	printf "$CORE_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$DRIVERS_PATH" ]; then
+	DRIVERS_INC="$(eval find "$DRIVERS_PATH" -type d -name 'Inc' -o -name 'Include' -o -name 'Legacy')"
+	printf "$DRIVERS_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$FATFS_PATH" ]; then
+	FATFS_INC="$(eval find "$FATFS_PATH" -type d)"
+	printf "$FATFS_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$RTOS_PATH" ]; then
+	RTOS_INC="$(eval find "$MIDWARE_PATH" -type d -name 'include' -o -name 'CMSIS_RTOS' -o -name 'ARM_CM4F')"
+	printf "$RTOS_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$MIDWARE_PATH" ]; then
+	MIDWARE_INC="$(eval find "$MIDWARE_PATH" -type d -name 'Inc' -o -name 'src')"
+	printf "$MIDWARE_INC\n" >>cmake/IncludeList.cmake
+fi
+
+if [ -n "$USB_PATH" ]; then
+	USB_INC="$(eval find "$USB_PATH" -type d)"
+	printf "$USB_INC\n" >>cmake/IncludeList.cmake
+fi
 
 # Remove Empty Line
 sed -i '/./!d' cmake/IncludeList.cmake
--- getSourceList.sh
+++ getSourceList.sh
@@ -2,30 +2,50 @@
 
 # Run .\getSourceList.sh in terminal to generate an updated list of source for CMake
 
+# Define the path to the source code
 APP_PATH='Application/*'
 CORE_PATH='Core/*'
 DRIVERS_PATH='Drivers/*'
-FATFS_PATH='FATFS/*'
+# FATFS_PATH='FATFS/*'
 MIDWARE_PATH='Middlewares/*'
-USB_PATH='USB_Device/*'
+# USB_PATH='USB_Device/*'
 
-APP_SRC="$(eval find "$APP_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-CORE_SRC="$(eval find "$CORE_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-DRIVERS_SRC="$(eval find "$DRIVERS_PATH" -type f -name '*.c')"
-FATFS_SRC="$(eval find "$FATFS_PATH" -type f -name '*.c')"
-MIDWARE_SRC="$(eval find "$MIDWARE_PATH" -type f -name '*.c')"
-USB_SRC="$(eval find "$USB_PATH" -type f -name '*.c')"
+# Clear the file
+> cmake/SourceList.cmake
 
 # Print find result
-printf "$APP_SRC\n$CORE_SRC\n$DRIVERS_SRC\n$FATFS_SRC\n$USB_SRC\n$MIDWARE_SRC" > cmake/SourceList.cmake
+if [ -n "$APP_PATH" ]; then
+	APP_SRC="$(eval find "$APP_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
+	printf "$APP_SRC\n" >> cmake/SourceList.cmake
+fi
 
-# Total Scan Method Has bug
-#ALL_SRC="$(eval find * ! -wholename 'build/*' -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
-#printf "$ALL_SRC" > test.txt
+if [ -n "$CORE_PATH" ]; then
+	CORE_SRC="$(eval find "$CORE_PATH" -type f -name '*.c' -o -name '*.cpp' -o -name '*.s')"
+	printf "$CORE_SRC\n" >> cmake/SourceList.cmake
+fi
 
+if [ -n "$DRIVERS_PATH" ]; then
+	DRIVERS_SRC="$(eval find "$DRIVERS_PATH" -type f -name '*.c')"
+	printf "$DRIVERS_SRC\n" >> cmake/SourceList.cmake
+fi
+
+if [ -n "$FATFS_PATH" ]; then
+	FATFS_SRC="$(eval find "$FATFS_PATH" -type f -name '*.c')"
+	printf "$FATFS_SRC\n" >> cmake/SourceList.cmake
+fi
+
+if [ -n "$MIDWARE_PATH" ]; then
+	MIDWARE_SRC="$(eval find "$MIDWARE_PATH" -type f -name '*.c')"
+	printf "$MIDWARE_SRC\n" >> cmake/SourceList.cmake
+fi
+
+if [ -n "$USB_PATH" ]; then
+	USB_SRC="$(eval find "$USB_PATH" -type f -name '*.c')"
+	printf "$USB_SRC\n" >> cmake/SourceList.cmake
+fi
 
 # Remove Empty Line
-sed -i '/./!d' cmake/SourceList.cmake
+sed -i '/./!d' cmake/IncludeList.cmake
 
 # Adding ${PROJ_PATH}/ to beginning of each line
 sed -i 's/^/${PROJ_PATH}\//' cmake/SourceList.cmake
